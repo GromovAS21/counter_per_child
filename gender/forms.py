7a758564f0d1a318bb662c86487a1228d0fe2535
Anton Gromov <seed14112@yandex.ru>
2025-08-09 19:38:55 +0300

    [~] Изменен GenderUpdateForm

--- gender/forms.py
+++ gender/forms.py
@@ -15,6 +15,6 @@ class GenderUpdateForm(ModelForm):
     def clean_amount(self):
         """Проверка на ввод данных"""
         amount = self.cleaned_data["amount"]
-        if amount < 0 or self.cleaned_data["amount"] == 0:
-            raise forms.ValidationError("Сумма не может быть меньше или равна 0.")
+        if amount < 0:
+            raise forms.ValidationError("Сумма не может быть меньше 0.")
         return amount
